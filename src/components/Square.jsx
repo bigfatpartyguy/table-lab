9ed10bfd30b06a8f26bd890a6148d3cbb36dc467
Maxim Morozov <maxmorozov.art@gmail.com>
2020-04-06 15:28:57 +0300

    initial commit

---
+++ src/components/Square.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default function Square(props) {
+  return (
+    <button onClick={props.onClick} className="square">
+      {props.value}
+    </button>
+  );
+}
